3c96de962ac6f662f5ddc40213961fa7b49c8fc5
kubra <kubracavuss@gmail.com>
2023-02-16 23:00:45 +0300

    changes

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -7,10 +7,14 @@ const Card = (data) => {
             <div className="title">
                 <h1>{data.title}</h1>
             </div>
-            <div>
-                <h1>
-                    {}
-                </h1>
+            <div className="date">
+                <h1>{data.date}</h1>
+            </div>
+            <img src={data.image} alt="" />
+            <div className="card-over">
+                <p>
+                    {data.desc}
+                </p>
             </div>
         </div>
     )
